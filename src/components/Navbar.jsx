bb0d4bbe4ecd5c8817d02c6f6ba368602547005a
dineshsingh099 <ddddineshrawatddd@gmail.com>
2026-02-13 03:06:10 +0530

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            
+            <navbar className='bg-blue-900 shadow-lg flex items-center justify-start py-1 px-6 fixed top-0 left-0 w-full'>
+                <h2 className='text-white font-bold p-4 text-xl'>Karban Board</h2>
+            </navbar>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold text-blue-600">Home Page</h1>
-		</div>
+		<>
+		<Navbar />
+		</>
 	);
 };
 
